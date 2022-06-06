635b2c702a8f2e998a4ea6b0fb789846cc16c255
maryanne-rose <rosemary.njeri@student.moringaschool.com>
2022-06-07 01:03:29 +0300

    commit tree 88ca98da9a67339d7afb5ab13f10ec4e140e3681

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue){
   // Write your code here!
+  if (someValue === 199) {
+    return 'This one is on me!';
+
+  } else if (someValue > 2000) {
+    return 'I will gladly take your thirty bucks.';
+
+  } else if (someValue >= 2501) {
+    return 'No can do.';
+
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return (city ? 'Ok, sounds good.' : 'No go.');
+
 }
 
-function switchOnCharmFromTip(){
+
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+      break;
+
+    case 'not as generous':
+      return 'Thank you.';
+      break;
+  
+    default:
+      return 'Bye.';
+  }
 }
